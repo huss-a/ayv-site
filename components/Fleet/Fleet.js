90d27b3d74a67e34aa88bb233403e353318b270c
Hussain Aqeel <hussaingoodboi@gmail.com>
2021-04-10 12:21:29 +0300

    test

--- components/Fleet/Fleet.js
+++ components/Fleet/Fleet.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Container, Form } from "react-bootstrap";
 import FleetCard from "./Card";
 import FleetList from "../../data/FleetList";
