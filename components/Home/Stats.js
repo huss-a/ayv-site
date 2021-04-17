b6feed3c146daea7407aa9197347e9950b243679
Hussain Aqeel <hussaingoodboi@gmail.com>
2021-04-17 16:23:51 +0300

    stats countup anim

--- components/Home/Stats.js
+++ components/Home/Stats.js
@@ -1,3 +1,5 @@
+import CountUp from "react-countup";
+
 const Stats = () => {
   return (
     <div
@@ -12,25 +14,25 @@ const Stats = () => {
           <h4>
             Flown Hours <i className="fas fa-clock" />
           </h4>
-          <h5>3,000+</h5>
+          <h5><CountUp duration={1.5} start={0} end={3000} suffix="+"/></h5>
         </div>
         <div className="total-pilots">
           <h4>
             Pilots <i className="fas fa-address-card" />
           </h4>
-          <h5>40+</h5>
+          <h5><CountUp duration={1.5} start={0} end={40} suffix="+" delay={1.5}/></h5>
         </div>
         <div className="total-miles">
           <h4>
             Miles Flown <i className="fas fa-plane-departure" />
           </h4>
-          <h5>8,54,000+</h5>
+          <h5><CountUp duration={1.5} start={0} end={854000} suffix="+" delay={3}/></h5>
         </div>
         <div className="total-routes">
           <h4>
             Routes <i className="fas fa-route" />
           </h4>
-          <h5>1850+</h5>
+          <h5><CountUp duration={1.5} start={0} end={1850} suffix="+" delay={4.5}/></h5>
         </div>
       </div>
     </div>
