d46493f19b5ad50c12dda2a1f9bd996be22950e3
Hussain Aqeel <hussaingoodboi@gmail.com>
2021-04-17 15:31:36 +0300

    va stats

--- pages/index.js
+++ pages/index.js
@@ -14,13 +14,33 @@ const Home = () => {
         <title>Finnair Virtual</title>
         <meta name="description" content="Finnair Virtual - Fly Finnish" />
       </Head>
-        <div className="main-banner">
-          <h1>Finnair Virtual</h1>
+      <div className="main-banner">
+        <h1>Finnair Virtual</h1>
+      </div>
+      <Container style={{ minHeight: "1000px" }}>
+        <div className="stats-container">
+          <h1>VA Stats</h1>
+          <div className="stats">
+            <div className="total-hrs">
+              <h4>Flown Hours <i className="fas fa-clock"/></h4>
+              <h5>3,000+</h5>
+            </div>
+            <div className="total-pilots">
+              <h4>Pilots <i className="fas fa-address-card"/></h4>
+              <h5>40+</h5>
+            </div>
+            <div className="total-miles">
+              <h4>Miles Flown <i className="fas fa-plane-departure"/></h4>
+              <h5>8,54,000+</h5>
+            </div>
+            <div className="total-routes">
+              <h4>Routes <i className="fas fa-route"/></h4>
+              <h5>1850+</h5>
+            </div>
+          </div>
         </div>
-        <Container style={{ minHeight: "1000px" }}>
-          <LiveFlights />
-          
-        </Container>
+        <LiveFlights />
+      </Container>
     </>
   );
 };
