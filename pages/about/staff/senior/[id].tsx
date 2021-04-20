0e155570cb76022abd34294c7367367a13eda574
Hussain Aqeel <hussaingoodboi@gmail.com>
2021-04-20 12:18:00 +0300

    senior [id] typed

--- pages/about/staff/senior/[id].tsx
+++ pages/about/staff/senior/[id].tsx
@@ -2,10 +2,16 @@ import staffList from "../../../../data/StaffList";
 import Head from "next/head";
 import { Container, Button } from "react-bootstrap";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+import { StaffInfo } from "../../../../data/StaffList";
 
-const staffMember = ({ member, memberAvatar }) => {
-  member = member[0];
+interface Props {
+  member: [StaffInfo];
+  memberAvatar: string;
+}
+
+const staffMember: React.FC<Props> = ({ member, memberAvatar }) => {
+  let singleMember = member[0];
   const router = useRouter();
   useEffect(() => {
     document.querySelector(".nav-about").classList.add("active");
@@ -15,7 +21,7 @@ const staffMember = ({ member, memberAvatar }) => {
   return (
     <>
       <Head>
-        <title>Finnair Virtual | Staff - {member.name}</title>
+        <title>Finnair Virtual | Staff - {singleMember.name}</title>
       </Head>
 
       <Container>
@@ -23,31 +29,33 @@ const staffMember = ({ member, memberAvatar }) => {
           <div className="staff-member-container">
             <img src={memberAvatar} className="pfp my-3" />
 
-            <h1 className="text-center">{member.name}</h1>
+            <h1 className="text-center">{singleMember.name}</h1>
             <h6 className="text-muted text-center">
-              <i className="fas fa-map-marker-alt" /> {member.location}
+              <i className="fas fa-map-marker-alt" /> {singleMember.location}
             </h6>
             <h6 className="text-center" style={{ color: "#0b1560" }}>
               <i className="fas fa-check" /> Senior Management
             </h6>
             <h6 className="text-center" style={{ color: "#0b1560" }}>
-              <i className="fas fa-check" /> {member.role}
+              <i className="fas fa-check" /> {singleMember.role}
             </h6>
-            <p className="text-center staff-member-bio my-2">{member.desc}</p>
+            <p className="text-center staff-member-bio my-2">
+              {singleMember.desc}
+            </p>
             <a
-              href={`https://community.infinitelflight.com/u/${member.ifcName}`}
+              href={`https://community.infinitelflight.com/u/${singleMember.ifcName}`}
               className="link"
             >
               <h6>IFC Profile</h6>
             </a>
-            {member.social && (
+            {singleMember.social && (
               <div className="social my-4">
                 <h4>Social Links</h4>
                 <div className="social-icons">
-                  {member.social.yt && (
+                  {singleMember.social.yt && (
                     <a
                       className="link"
-                      href={member.social.yt}
+                      href={singleMember.social.yt}
                       target="_blank"
                       rel="noreferrer"
                     >
@@ -59,10 +67,10 @@ const staffMember = ({ member, memberAvatar }) => {
                       </h4>
                     </a>
                   )}
-                  {member.social.ig && (
+                  {singleMember.social.ig && (
                     <a
                       className="link"
-                      href={member.social.ig}
+                      href={singleMember.social.ig}
                       target="_blank"
                       rel="noreferrer"
                     >
@@ -74,10 +82,10 @@ const staffMember = ({ member, memberAvatar }) => {
                       </h4>
                     </a>
                   )}
-                  {member.social.github && (
+                  {singleMember.social.github && (
                     <a
                       className="link"
-                      href={member.social.github}
+                      href={singleMember.social.github}
                       target="_blank"
                       rel="noreferrer"
                     >
@@ -110,7 +118,9 @@ export default staffMember;
 export async function getStaticProps({ params }) {
   //Filter the senior management array and return it as a prop
 
-  let member = staffList.seniorManagement.filter((mem) => mem.id == params.id);
+  let member: StaffInfo[] = staffList.seniorManagement.filter(
+    (mem) => mem.id == params.id
+  )!;
   const res = await fetch(
     `https://community.infiniteflight.com/u/${member[0].ifcName}.json`
   );
