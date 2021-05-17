8415198311042c6204433349f8d5f6d6cf7f4e82
Hussain Aqeel <hussaingoodboi@gmail.com>
2021-05-17 12:30:18 +0300

    useFetch rm

--- client/components/Home/LiveFlights.tsx
+++ client/components/Home/LiveFlights.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState, useCallback } from "react";
 import { Spinner, Button } from "react-bootstrap";
-import useFetch from "../../helpers/useFetch";
+
 /*
 Enviroment Variables-
 
