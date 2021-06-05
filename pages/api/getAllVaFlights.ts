466a1fd11e4271f1caf12c7042c4b051d57c7f16
Hussain Aqeel <hussaingoodboi@gmail.com>
2021-06-05 12:49:44 +0300

    test

--- pages/api/getAllVaFlights.ts
+++ pages/api/getAllVaFlights.ts
@@ -3,8 +3,8 @@ import { FlightInfo } from "../../types/FlightInfo";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.headers.referer !== process.env.NEXT_PUBLIC_SITE_URL + "/")
-    return res.status(403).json({ msg: "Unauthorized request." });
+  // if (req.headers.referer !== process.env.NEXT_PUBLIC_SITE_URL + "/")
+  //   return res.status(403).json({ msg: "Unauthorized request." });
 
   const callsignRegex = /^Finnair \d{3}VA$/g;
   const url = `https://api.infiniteflight.com/public/v2/flights/${process.env.NEXT_PUBLIC_SESSION_ID}`;
